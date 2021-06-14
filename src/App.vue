c978ac457db0c88ec33df4f31a1e7211b2c74518
Denis <44744142+ashlanderDesign@users.noreply.github.com>
2021-06-15 00:40:07 +0500

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,365 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <header class="header-wrapper">
+      <div class="header-top">
+        <button class="button button-primary-transparent">
+          <img src="/img/navigation_arrow.svg" alt="" class="button-icon" />
+          <span class="button-label">Курган</span>
+        </button>
+        <div class="header-top-right">
+          <a href="#top">Магазины</a>
+          <a href="#top" id="login-button">Личный кабинет</a>
+        </div>
+        <div id="loginForm" class="header-login-form hidden">
+          <h4 class="header-login-form-title">Войти или зарегистрироваться</h4>
+          <input type="text" placeholder="Имя пользователя" />
+          <input type="password" placeholder="Пароль" />
+          <button class="button button-primary button-block button-medium">
+            Войти
+          </button>
+          <button
+            id="registerButton"
+            class="button button-secondary button-block button-medium"
+          >
+            Зарегистрироваться
+          </button>
+        </div>
+        <div id="registerForm" class="header-login-form hidden">
+          <h4 class="header-login-form-title">Регистрация</h4>
+          <input type="text" placeholder="Имя пользователя" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Пароль" />
+          <input type="password" placeholder="Повторите пароль" />
+          <button class="button button-primary button-block button-medium">
+            Зарегистрироваться
+          </button>
+          <button
+            id="haveAccountButton"
+            class="button button-secondary button-block button-medium"
+          >
+            У меня уже есть аккаунт
+          </button>
+        </div>
+      </div>
+      <div class="header-mid">
+        <img src="/img/logo_akva.jpg" alt="" class="header-logo" />
+        <div class="search">
+          <input
+            type="text"
+            placeholder="Поиск по каталогу..."
+            class="header-search"
+          />
+          <button class="button button-circle button-large button-primary">
+            <img class="button-icon" src="/img/search_icon.svg" alt="" />
+          </button>
+        </div>
+        <section class="header-phone">
+          <img src="/img/phone_icon.svg" alt="" />
+          <a href="tel:+78009999999">+7 800 999 99-99</a>
+        </section>
+        <button class="button button-large button-primary-transparent">
+          <img src="/img/cart_icon.svg" alt="" class="button-icon" />
+          <span class="button-label">Корзина</span>
+        </button>
+      </div>
+      <div class="header-nav">
+        <button class="button button-large-wide button-primary" id="catalog">
+          <img src="/img/hamburger_icon.svg" alt="" class="button-icon" />
+          <span class="button-label">Каталог товаров</span>
+        </button>
+        <a href="">Акции</a><a href="">Доставка и самовывоз</a
+        ><a href="">Установка</a><a href="">Вакансии</a
+        ><a href="">Юридическим лицам</a>
+        <section class="catalog-list hidden">
+          <div class="list-item">
+            <span>Абразивные материалы</span>
+            <img src="/img/chevron_right.svg" alt="" class="list-item-icon" />
+            <div class="list-item-children hidden">
+              <div class="list-item">Круги зачистные</div>
+              <div class="list-item">Круги отрезные по металлу</div>
+            </div>
+          </div>
+          <div class="list-item">
+            <span>Вентиляционные системы</span
+            ><img src="/img/chevron_right.svg" alt="" class="list-item-icon" />
+            <div class="list-item-children hidden">
+              <div class="list-item">Вентиляция AURAMAX</div>
+              <div class="list-item">Вентиляция ФЛЕКС</div>
+              <div class="list-item">Вентиляция ЭРА</div>
+              <div class="list-item">Люки ревизионные LYUKER</div>
+              <div class="list-item">Фурнитура для воздуховодов</div>
+            </div>
+          </div>
+          <div class="list-item">
+            <span>Водосливная арматура</span
+            ><img src="/img/chevron_right.svg" alt="" class="list-item-icon" />
+            <div class="list-item-children hidden">
+              <div class="list-item">Водосливная арматура АНИ ПЛАСТ</div>
+              <div class="list-item">Водосливная арматура ALCAPLAST</div>
+              <div class="list-item">Водосливная арматура UNICORN</div>
+              <div class="list-item">Водосливная арматура АКВАТЕР</div>
+              <div class="list-item">Водосливная арматура разное</div>
+              <div class="list-item">Шланги д/стиральных машин</div>
+              <div class="list-item">
+                Шланги д/стиральных машин в индивидуальной упаковке
+              </div>
+            </div>
+          </div>
+          <div class="list-item">
+            <span>Смывная арматура</span
+            ><img src="/img/chevron_right.svg" alt="" class="list-item-icon" />
+            <div class="list-item-children hidden"></div>
+          </div>
+          <div class="list-item">
+            <span>Резинотехнические изделия</span
+            ><img src="/img/chevron_right.svg" alt="" class="list-item-icon" />
+            <div class="list-item-children hidden"></div>
+          </div>
+          <div class="list-item">
+            <span>Канализационные системы</span
+            ><img src="/img/chevron_right.svg" alt="" class="list-item-icon" />
+            <div class="list-item-children hidden"></div>
+          </div>
+          <div class="list-item">
+            <span>Радиаторы и комплектующие</span
+            ><img src="/img/chevron_right.svg" alt="" class="list-item-icon" />
+            <div class="list-item-children hidden"></div>
+          </div>
+          <div class="list-item">
+            <span>Водонагреватели, котлы, электрические теплые полы</span
+            ><img src="/img/chevron_right.svg" alt="" class="list-item-icon" />
+            <div class="list-item-children hidden"></div>
+          </div>
+        </section>
+      </div>
+    </header>
+    <div class="container">
+      <router-view />
     </div>
-    <router-view />
+    <footer class="footer-wrapper">
+      <div class="footer-top">
+        <img src="/img/logo_akva.jpg" alt="" class="footer-logo" />
+        <a href="">Связаться с нами</a>
+        <a href="">Заказать обратный звонок</a>
+        <a href="">Публичная оферта</a>
+        <a href="">Политика конфеденциальности</a>
+      </div>
+      <!-- <div class="footer-bottom">2021 &copy; АКВА</div> -->
+    </footer>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import "sass/button";
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 
-#nav {
-  padding: 30px;
+#app {
+  background-image: url("/img/geometry.png");
+  background-size: 256px 256px;
+  font-family: "Roboto", sans-serif;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.header-wrapper {
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  width: 100%;
+  font-family: "Roboto", sans-serif;
+  min-height: 15vh;
+  background-color: #fff;
+  box-shadow: 0 0 60px rgba(0, 0, 0, 0.12);
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.header-top a {
+  text-decoration: none;
+  color: #3c3c3a;
+}
+
+.header-top a:hover {
+  color: #2075f5;
+}
+
+.header-top {
+  display: flex;
+  justify-content: space-between;
+  padding: 12px 40px;
+  align-items: center;
+  width: 100%;
+  background-color: #cdd9e9;
+  position: relative;
+}
+
+.header-top-right {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+}
+
+.header-login-form {
+  position: absolute;
+  background-color: #fff;
+  padding: 16px;
+  border-radius: 16px;
+  top: calc(100% + 16px);
+  right: 32px;
+  box-shadow: 0 0 13px rgba(0, 0, 0, 0.23);
+  z-index: 9999;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 8px;
+  flex-direction: column;
+  max-width: 15vw;
+}
+
+.header-login-form-title {
+  font-size: 24px;
+  text-align: center;
+  margin-bottom: 16px;
+}
+
+.header-login-form input {
+  width: 100%;
+  padding: 8px 16px;
+  border-radius: 8px;
+  outline: none;
+  border: none;
+  background-color: #ebebeb;
+}
+
+.header-mid {
+  width: 100%;
+  padding: 12px 40px;
+  background-color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.header-nav {
+  width: 100%;
+  padding: 12px 40px;
+  background-color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 64px;
+  position: relative;
+}
+
+.header-nav a {
+  color: #3a3a3c;
+  text-decoration: none;
+  font-weight: 700;
+  font-size: 18px;
+}
+
+.header-nav a:hover {
+  color: #2075f5;
+}
+
+.search {
+  width: 70%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+}
+
+.header-search {
+  width: 80%;
+  background-color: #ebebeb;
+  border-radius: 16px;
+  padding: 16px 18px;
+  outline: none;
+  border: none;
+}
+
+.header-search-button {
+  background-color: #2075f5;
+  border-radius: 50%;
+  height: 32px;
+  width: 32px;
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.header-phone {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 8px;
+  padding: 8px 0;
+  border-radius: 16px;
+}
+
+.header-phone img {
+  height: 24px;
+}
+
+.header-phone a {
+  font-weight: 700;
+  font-size: 24px;
+  color: #000;
+  text-decoration: none;
+}
+
+.header-cart {
+  height: 32px;
+}
+
+.header-search-button img {
+  width: 16px;
+}
+
+.header-logo {
+  height: 9vh;
+}
+
+.hidden {
+  display: none;
+}
+
+.footer-wrapper {
+  background-color: #cfcfcf;
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  flex-direction: column;
+  padding: 8px 40px;
+  gap: 8px;
+}
+
+.footer-logo {
+  height: 8vh;
+}
+
+.footer-top a {
+  text-decoration: none;
+  font-size: 18px;
+  color: #f5f5f5;
+  background-color: #2075f5;
+  padding: 8px 16px;
+  border-radius: 8px;
+}
+
+.footer-top a:hover {
+  background-color: #1d62ca;
+}
+
+.footer-top {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,200 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <section class="landing" id="home">
+    <div class="banner">
+      <h2>Установим технику за Вас</h2>
+      <button class="button button-large button-primary">
+        Рассчитать установку
+      </button>
+    </div>
+    <h1 class="title">Рекомендуем Вам</h1>
+    <div class="cards-recommend">
+      <div class="card">
+        <img src="" alt="" class="card-image" />
+        <div class="card-body">
+          <h3 class="card-header">Канал круглый 1,0 м D125 ФЛЕКС</h3>
+          <span class="card-stock card-stock-green">В наличии</span>
+        </div>
+        <div class="card-footer">
+          <button class="button button-large button-block button-primary">
+            Купить
+          </button>
+          <span class="card-price">1400 ₽</span>
+        </div>
+      </div>
+      <div class="card">
+        <img src="" alt="" class="card-image" />
+        <div class="card-body">
+          <h3 class="card-header">
+            Колено разноугловое горизонтальное 60х120 ФЛЕКС
+          </h3>
+          <span class="card-stock card-stock-green">В наличии</span>
+        </div>
+        <div class="card-footer">
+          <button class="button button-large button-block button-primary">
+            Купить
+          </button>
+          <span class="card-price">1400 ₽</span>
+        </div>
+      </div>
+      <div class="card">
+        <img src="" alt="" class="card-image" />
+        <div class="card-body">
+          <h3 class="card-header">
+            Сифон 1 1,2х40 Ani Грот с гофрой 40х40х50 А0115 АНИ пласт
+          </h3>
+          <span class="card-stock card-stock-green">В наличии</span>
+        </div>
+        <div class="card-footer">
+          <button class="button button-large button-block button-primary">
+            Купить
+          </button>
+          <span class="card-price">1400 ₽</span>
+        </div>
+      </div>
+      <div class="card">
+        <img src="" alt="" class="card-image" />
+        <div class="card-body">
+          <h3 class="card-header">
+            Сифон 1 1,2х40 А220 с гофрой 40х40х50 UNICORN
+          </h3>
+          <span class="card-stock card-stock-green">В наличии</span>
+        </div>
+        <div class="card-footer">
+          <button class="button button-large button-block button-primary">
+            Купить
+          </button>
+          <span class="card-price">1400 ₽</span>
+        </div>
+      </div>
+      <div class="card">
+        <img src="" alt="" class="card-image" />
+        <div class="card-body">
+          <h3 class="card-header">
+            Соединитель 55х110,D100 эксц 90 град круглый ФЛЕКС
+          </h3>
+          <span class="card-stock card-stock-green">В наличии</span>
+        </div>
+        <div class="card-footer">
+          <button class="button button-large button-block button-primary">
+            Купить
+          </button>
+          <span class="card-price">1400 ₽</span>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
+
+<style lang="scss" scoped>
+@import "../sass/button";
+
+.cards-recommend {
+  display: grid;
+  grid-template-columns: repeat(5, 4fr);
+  grid-template-rows: repeat(1, 1fr);
+  gap: 16px;
+}
+
+.banner {
+  width: 100%;
+  background-image: url("/img/santehnik.jpg");
+  background-size: cover;
+  border-radius: 16px;
+  height: 35vh;
+  height: 100%;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  flex-direction: column;
+  gap: 16px;
+  padding: 32px 150px;
+  color: #fff;
+}
+
+.banner h2 {
+  color: #fff;
+  font-size: 56px;
+  text-shadow: 0 0 5px rgba(0, 0, 0, 0.4);
+}
+
+.landing {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 2fr 0.5fr 4fr;
+  gap: 32px;
+  padding: 32px 40px;
+}
+
+.card {
+  display: grid;
+  width: 100%;
+  height: 100%;
+  grid-template-columns: 1fr;
+  grid-template-rows: 4fr 2fr 0.5fr;
+  background-color: #fff;
+  box-shadow: 0 0 6px rgba(0, 0, 0, 0.08);
+  padding: 16px;
+  border-radius: 4px;
+  gap: 16px;
+}
+
+.card-image {
+  width: 100%;
+}
+
+.card-body {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  flex-direction: column;
+}
+
+.card-footer {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 16px;
+  background-color: #eeeeee;
+  padding: 8px;
+  border-radius: 8px;
+}
+
+.card-price {
+  padding: 16px;
+  border-radius: 8px;
+  font-size: 18px;
+  font-weight: 500;
+  width: 40%;
+  white-space: nowrap;
+}
+
+.card-header {
+  font-weight: 600;
+  letter-spacing: 0.8px;
+  font-size: 18px;
+  line-height: 20px;
+  cursor: pointer;
+}
+
+.card-header:hover {
+  color: #2075f5;
+}
+
+.card-stock {
+  font-weight: 700;
+}
+
+.card-stock-green {
+  color: rgb(11, 155, 11);
+}
+
+.card-stock-red {
+  color: rgb(224, 26, 19);
+}
+</style>
